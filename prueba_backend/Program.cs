5eed72e43e4352c8fd6bb29d22cba6aebfe0bb1f
Leyan505 <leyan50505@gmail.com>
2024-11-28 22:08:57 -0600

    feat: add deposit and withdraw endpoints with changes to the validation logic and methods now you will be able to detect if a bank account exists, add classes for bank accounts and transactions

--- prueba_backend/Program.cs
+++ prueba_backend/Program.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.OpenApi.Services;
 using Microsoft.VisualBasic;
+using prueba_backend;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +23,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-
+//get and create bank accounts
 app.MapGet("/accounts", (IBankAccountService service) => service.GetBankAccounts());
 app.MapPost("/accounts", (BankAccount newAccount, IBankAccountService service) =>
 {
@@ -29,6 +31,7 @@ app.MapPost("/accounts", (BankAccount newAccount, IBankAccountService service) =
     return TypedResults.Created("/accounts/{id}", newAccount);
 }).AddEndpointFilter(async (context, next) => {
     var accountArgument = context.GetArgument<BankAccount>(0);
+    var service = context.GetArgument<InMemoryBankAccountService>(1);
     var errors = new Dictionary<string, string[]>();
 
     if (accountArgument.Balance < 0)
@@ -41,7 +44,6 @@ app.MapPost("/accounts", (BankAccount newAccount, IBankAccountService service) =
     }
     else 
     {
-        var service = context.GetArgument<InMemoryBankAccountService>(1);
         if (service.Duplicate(accountArgument.AccountNumber) == true)
         {
             errors.Add(nameof(accountArgument.AccountNumber), ["Duplicate account number is not allowed."]);
@@ -56,17 +58,24 @@ app.MapPost("/accounts", (BankAccount newAccount, IBankAccountService service) =
     return await next(context);
 });
 
+
+//Check balance given an account number
 app.MapGet("/check_balance/{accountNumber}", (string accountNumber, IBankAccountService service) =>
 {
     var balance = service.CheckBalance(accountNumber);
-    return TypedResults.Ok(balance);
+    var response = new{
+        accountNumber = accountNumber,
+        balance = balance  
+    };
+    return TypedResults.Ok(response);
 
 })
 .AddEndpointFilter(async (context, next) => {
     var accountNumber = context.GetArgument<string>(0);
+    var service = context.GetArgument<InMemoryBankAccountService>(1);
     var errors = new Dictionary<string, string[]>();
 
-    if(accountNumber == null || accountNumber.Length != 20 || !accountNumber.All(char.IsDigit))
+    if(!service.AccountIsValid(accountNumber))
     {
         errors.Add(nameof(accountNumber), ["Invalid account number."]);
     }
@@ -79,42 +88,219 @@ app.MapGet("/check_balance/{accountNumber}", (string accountNumber, IBankAccount
     return await next(context);
 });
 
+
+//Deposit to a given account
+app.MapPost("/deposit", (TransactionDTO newTransaction, IBankAccountService service) =>
+{
+    var transaction = service.Deposit(newTransaction);
+    return TypedResults.Created("/transactions/{id}", transaction);
+})
+.AddEndpointFilter(async (context, next) => {
+    var transactionArgument = context.GetArgument<TransactionDTO>(0);
+    var service = context.GetArgument<InMemoryBankAccountService>(1);
+    var errors = new Dictionary<string, string[]>();
+
+    if(!service.AccountIsValid(transactionArgument.AccountNumber))
+    {
+        errors.Add(nameof(transactionArgument.AccountNumber), ["Invalid account number."]);
+    }
+
+    if(transactionArgument.Amount <= 0)
+    {
+        errors.Add(nameof(transactionArgument.Amount), ["Invalid amount, must be postive."]);
+    }
+
+    if(errors.Count > 0)
+    {
+        return Results.ValidationProblem(errors);
+    }
+
+    return await next(context);
+});
+
+
+//Withdraw from a given account
+app.MapPost("/withdraw", (TransactionDTO newTransaction, IBankAccountService service) =>
+{
+    var transaction = service.Withdraw(newTransaction);
+    return TypedResults.Created("/transactions/{id}", transaction);
+})
+.AddEndpointFilter(async (context, next) => {
+    var transactionArgument = context.GetArgument<TransactionDTO>(0);
+    var service = context.GetArgument<InMemoryBankAccountService>(1);
+    var errors = new Dictionary<string, string[]>();
+
+    if(!service.AccountIsValid(transactionArgument.AccountNumber))
+    {
+        errors.Add(nameof(transactionArgument.AccountNumber), ["Invalid account number."]);
+    }
+
+    if(transactionArgument.Amount <= 0)
+    {
+        errors.Add(nameof(transactionArgument.Amount), ["Invalid amount, must be postive."]);
+    }
+    if(transactionArgument.AccountNumber != null)
+    {
+        if((service.CheckBalance(transactionArgument.AccountNumber) - transactionArgument.Amount) < 0)
+        {
+            errors.Add(nameof(transactionArgument.Amount), ["Not enough balance."]);
+        }
+    }
+
+    if(errors.Count > 0)
+    {
+        return Results.ValidationProblem(errors);
+    }
+
+    return await next(context);
+});
+
+//Get all transactions
+app.MapGet("/transactions/{accountNumber}", (string accountNumber, IBankAccountService service) => {
+    var response = new {
+        transactions = service.GetTransactionsByAccount(accountNumber),
+        final_balance = service.CheckBalance(accountNumber)
+    };
+    return response;
+    
+    });
+
 app.Run();
 
-public record BankAccount(string AccountNumber, double Balance);
-
-interface IBankAccountService
+namespace prueba_backend
 {
-    List<BankAccount> GetBankAccounts();
-    BankAccount AddAccount(BankAccount account);
-    bool Duplicate(string accountNumber);
-    double? CheckBalance(string accountNumber);
-
-}
-
-class InMemoryBankAccountService : IBankAccountService
-{
-    private readonly List<BankAccount> _accounts = [];
-    public List<BankAccount> GetBankAccounts()
+    public class BankAccount{
+        public required string AccountNumber {get; set;}
+        public required double Balance {get; set;}
+    };
+    public class Transaction
     {
-        return _accounts;
+        private static int _idCounter = 0;
+        public int Id {get; private set;}
+        public required string Type {get; set;}
+        public required double Amount {get; set;}
+        public required string AccountNumber {get; set;}
+        public required double SaldoResultante {get; set;}
+        public Transaction(string Type, double Amount, string AccountNumber, double SaldoResultante )
+        {
+            Id = _idCounter + 1;
+            this.Type = Type;
+            this.Amount = Amount;
+            this.AccountNumber = AccountNumber;
+            this.SaldoResultante = SaldoResultante;
+
+        }
+
+        public Transaction()
+        {
+            Id = ++_idCounter;
+        }
     }
 
-    public BankAccount AddAccount(BankAccount account)
-    {
-        _accounts.Add(account);
-        return account;
+    public class TransactionDTO{
+        public required double Amount {get; set;}
+        public required string AccountNumber {get; set;}
     }
 
-    public bool Duplicate(string accountNumber)
+    interface IBankAccountService
     {
-        return _accounts.Find(x => x.AccountNumber.Equals(accountNumber)) != null;
-    } 
+        List<BankAccount> GetBankAccounts();
+        List<Transaction> GetTransactions();
+        List<Transaction> GetTransactionsByAccount(string accountNumber);
 
-    public double? CheckBalance(string accountNumber)
-    {
-        var account = _accounts.Find(x => x.AccountNumber.Equals(accountNumber));
-        return account?.Balance;
+
+        BankAccount AddAccount(BankAccount account);
+
+        bool Duplicate(string accountNumber);
+        double? CheckBalance(string accountNumber);
+        Transaction? Deposit(TransactionDTO newTransaction);
+        Transaction? Withdraw(TransactionDTO newTransaction);
+
+        public bool AccountExists(string accountNumber);
+        public bool AccountIsValid(string accountNumber);
     }
 
+    class InMemoryBankAccountService : IBankAccountService
+    {
+        private readonly List<BankAccount> _accounts = [];
+        private readonly List<Transaction> _transactions = [];
+        public List<BankAccount> GetBankAccounts()
+        {
+            return _accounts;
+        }
+
+        public BankAccount AddAccount(BankAccount account)
+        {
+            _accounts.Add(account);
+            return account;
+        }
+
+        public bool Duplicate(string accountNumber)
+        {
+            return _accounts.Find(x => x.AccountNumber.Equals(accountNumber)) != null;
+        } 
+
+        public double? CheckBalance(string accountNumber)
+        {
+            var account = _accounts.Find(x => x.AccountNumber.Equals(accountNumber));
+            return account?.Balance;
+        }
+
+        public Transaction Deposit(TransactionDTO newTransaction)
+        {
+            var account = _accounts.Find(x => x.AccountNumber.Equals(newTransaction.AccountNumber));
+            
+            account.Balance += newTransaction.Amount;
+            var transaction = new Transaction(){
+                Amount = newTransaction.Amount,
+                AccountNumber = newTransaction.AccountNumber,
+                Type = "DEPOSIT",
+                SaldoResultante = account.Balance
+            };
+            _transactions.Add(transaction);
+            return transaction;
+        }
+
+        public Transaction Withdraw(TransactionDTO newTransaction)
+        {
+            var account = _accounts.Find(x => x.AccountNumber.Equals(newTransaction.AccountNumber));
+            
+            account.Balance -= newTransaction.Amount;
+            var transaction = new Transaction(){
+                Amount = newTransaction.Amount,
+                AccountNumber = newTransaction.AccountNumber,
+                Type = "WITHDRAW",
+                SaldoResultante = account.Balance
+            };
+            _transactions.Add(transaction);
+            return transaction;
+        }
+
+        public bool AccountExists(string accountNumber)
+        {
+            var account = _accounts.Find(x => x.AccountNumber.Equals(accountNumber));
+            return account != null;
+
+        }
+        public bool AccountIsValid(string accountNumber)
+        {
+            if (accountNumber == null || accountNumber.Length != 20 || !accountNumber.All(char.IsDigit) || !AccountExists(accountNumber))
+            {
+               return false;
+            }
+            return true;
+
+        }
+
+        public List<Transaction> GetTransactions()
+        {
+            return _transactions;
+        }
+
+        public List<Transaction> GetTransactionsByAccount(string accountNumber)
+        {
+            var transactions = _transactions.FindAll(x => x.AccountNumber.Equals(accountNumber));
+            return transactions;
+        }
+    }
 }
